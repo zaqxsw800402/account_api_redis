a45c7d7f5022265f82fad3ed9191bd72816c5025
zaqxssw79218@gmail.com <Aa29066110>
2021-12-08 23:37:49 +0800

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,103 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 	"red/cmd/api/gin_app"
+	"red/internal/driver"
+	"red/internal/models"
+	"time"
 )
 
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+	//smtp struct {
+	//	host     string
+	//	port     int
+	//	username string
+	//	password string
+	//}
+	secretKey string
+	frontend  string
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
+	DB       models.DBModel
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Printf("Starting Back end server in %s mode on port %d", app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
+
 func main() {
-	gin_app.Start()
-	fmt.Println("Start")
+	log.Println("Api Starting...")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production|maintenance}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "trevor:secret@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
+	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to frontend")
+
+	flag.Parse()
+
+	//flag.StringVar(&cfg.smtp.host, "smtphost", "smtp.mailtrap.io", "smtp host")
+	//flag.IntVar(&cfg.smtp.port, "smtp port", 587, "smtp port")
+	//cfg.smtp.username = os.Getenv("Username")
+	//cfg.smtp.password = os.Getenv("Password")
+	//cfg.secretKey = os.Getenv("Secret")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := driver.OpenDb(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		version:  version,
+		DB:       models.DBModel{DB: conn},
+	}
+
+	err = app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+
 }
